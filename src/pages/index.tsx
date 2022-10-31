62ac261c0d7f18da5bad8653d393fad6c6740ce1
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-10-31 21:26:08 +0200

    feat(titles): added headings
    added subheadings
    
    changed cards styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,13 @@ export default function IndexPage() {
   }
   return (
     <>
+      <Text align="center" fontSize="xl" mt={10}>
+        {" "}
+        Welcome to the world of Pokemon{" "}
+      </Text>
+      <Text align="center" fontSize="md">
+        We are just fans making a page{" "}
+      </Text>
       <Card />
     </>
   );
